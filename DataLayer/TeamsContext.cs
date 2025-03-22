281a8d69877e794ccad017fed19c94ca0c8ea262
DESKTOP-NBFVQNP\kmedov <kalata07m@abv.bg>
2025-03-22 10:07:57 +0200

    .

---
+++ DataLayer/TeamsContext.cs
@@ -0,0 +1,70 @@
+﻿using DataLayer.BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class TeamsContext : IDb<Team, int>
+    {
+        private FootballteamsContext dbContext;
+
+        public TeamsContext(FootballteamsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Team item)
+        {
+            dbContext.Teams.Add(item);
+            dbContext.SaveChanges();
+        }
+
+
+        public Team Read(int key, bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            IQueryable<Team> query = dbContext.Teams;
+
+            if (useNavigationalProperties) query = query.Include(g => g.CountryCode);
+            if (isReadOnly) query = query.AsNoTrackingWithIdentityResolution();
+
+            Team team = query.FirstOrDefault(g => g.Id == key);
+
+            if (team is null) throw new ArgumentException($"Team with id {key} does not exist!");
+
+            return team;
+        }
+
+        public List<Team> ReadAll(bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            IQueryable<Team> query = dbContext.Teams;
+
+            if (useNavigationalProperties) query = query.Include(g => g.CountryCode);
+            if (isReadOnly) query = query.AsNoTrackingWithIdentityResolution();
+
+            return query.ToList();
+        }
+
+        public void Update(Team item, bool useNavigationalProperties = false)
+        {
+            Team team = Read(item.Id, useNavigationalProperties);
+            if (team == null)
+            {
+                throw new ArgumentException("Team not found!");
+            }
+
+            dbContext.Entry(team).CurrentValues.SetValues(item);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(int key)
+        {
+            Team teamFromDb = Read(key);
+            dbContext.Teams.Remove(teamFromDb);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ DataLayer/TrophiesContext.cs
@@ -0,0 +1,70 @@
+﻿using DataLayer.BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class TrophiesContext : IDb<Trophy, int>
+    {
+        private FootballteamsContext dbContext;
+
+        public TrophiesContext(FootballteamsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Trophy item)
+        {
+            dbContext.Trophies.Add(item);
+            dbContext.SaveChanges();
+        }
+
+        public Trophy Read(int key, bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            IQueryable<Trophy> query = dbContext.Trophies;
+
+            if (useNavigationalProperties) query = query.Include(g => g.CountryCode).Include(g => g.ContinentCode);
+
+            if (isReadOnly) query = query.AsNoTrackingWithIdentityResolution();
+
+            Trophy trophy = query.FirstOrDefault(g => g.Id == key);
+
+            if (trophy is null) throw new ArgumentException($"Trophy with id {key} does not exist!");
+
+            return trophy;
+        }
+
+        public List<Trophy> ReadAll(bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            IQueryable<Trophy> query = dbContext.Trophies;
+
+            if (useNavigationalProperties) query = query.Include(g => g.CountryCode).Include(g => g.ContinentCode);
+            if (isReadOnly) query = query.AsNoTrackingWithIdentityResolution();
+
+            return query.ToList();
+        }
+
+        public void Update(Trophy item, bool useNavigationalProperties = false)
+        {
+            Trophy trophy = Read(item.Id, useNavigationalProperties);
+            if (trophy == null)
+            {
+                throw new ArgumentException("Trophy not found!");
+            }
+
+            dbContext.Entry(trophy).CurrentValues.SetValues(item);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(int key)
+        {
+            Trophy trophyFromDb = Read(key);
+            dbContext.Trophies.Remove(trophyFromDb);
+            dbContext.SaveChanges();
+        }
+    }
+}
