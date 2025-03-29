85e26ad915fea51df3442727d90ed842639704a9
DESKTOP-NBFVQNP\kmedov <kalata07m@abv.bg>
2025-03-29 11:22:43 +0200

    tests

---
+++ Test/UnitTest1.vb
@@ -0,0 +1,18 @@
+﻿Imports NUnit.Framework
+
+Namespace Test
+
+    Public Class Tests
+
+        <SetUp>
+        Public Sub Setup()
+        End Sub
+
+        <Test>
+        Public Sub Test1()
+            Assert.Pass()
+        End Sub
+
+    End Class
+
+End Namespace
