4599c7e60bb01d0ecf4122ef5b36dc3a1ef3ce26
DESKTOP-NBFVQNP\kmedov <kalata07m@abv.bg>
2025-03-29 14:45:58 +0200

    Tests

---
+++ Test/Tests/TestManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Tests
+{
+    [TestFixture]
+    public class TestManager
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
