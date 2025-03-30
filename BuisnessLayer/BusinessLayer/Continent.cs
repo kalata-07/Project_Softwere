2b6950f2af9770ea476d2d5e9f295624aa27a5a9
Kolina Spronok <teditenev14@gmail.com>
2025-03-30 12:18:59 +0300

    Added BuisnessLayer

---
+++ BuisnessLayer/BusinessLayer/Continent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.BusinessLayer;
+
+public partial class Continent
+{
+    public string ContinentCode { get; set; } = null!;
+
+    public string ContinentName { get; set; } = null!;
+
+    public virtual ICollection<Country> Countries { get; set; } = new List<Country>();
+
+    public virtual ICollection<Trophy> Trophies { get; set; } = new List<Trophy>();
+}
---
+++ BuisnessLayer/BusinessLayer/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.BusinessLayer;
+
+public partial class Country
+{
+    public string CountryCode { get; set; } = null!;
+
+    public string CountryName { get; set; } = null!;
+
+    public string ContinentCode { get; set; } = null!;
+
+    public virtual Continent ContinentCodeNavigation { get; set; } = null!;
+
+    public virtual ICollection<Footballer> Footballers { get; set; } = new List<Footballer>();
+
+    public virtual ICollection<Stadium> Stadium { get; set; } = new List<Stadium>();
+
+    public virtual ICollection<Team> Teams { get; set; } = new List<Team>();
+
+    public virtual ICollection<Trophy> Trophies { get; set; } = new List<Trophy>();
+}
