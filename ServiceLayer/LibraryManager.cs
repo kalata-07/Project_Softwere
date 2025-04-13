38261758a83f7c82240172575033e1e748e8136f
Kolina Spronok <teditenev14@gmail.com>
2025-04-13 14:22:47 +0300

    Added ServiceLayer

---
+++ ServiceLayer/LibraryManager.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer;
+
+
+namespace ServiceLayer
+{
+    public class LibraryManager<T, K>
+    {
+        private IDb<T, K> context;
+
+        public LibraryManager(IDb<T, K> context)
+        {
+            this.context = context;
+        }
+
+        public void Create(T item)
+        {
+            context.Create(item);
+        }
+
+        public T Read(K key, bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            return context.Read(key);
+        }
+        public List<T> ReadAll(bool useNavigationalProperties = false, bool isReadOnly = false)
+        {
+            return context.ReadAll();
+        }
+
+        public void Update(T item, bool useNavigationalProperties = false)
+        {
+            context.Update(item);
+        }
+
+        public void Delete(K key)
+        {
+            context.Delete(key);
+        }
+
+
+
+
+
+    }
+}
